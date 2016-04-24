4a4d856bce2f59ade93ed132b79c63d6b79b0bef
Jonathan Vogt <code@v-o-g-t.net>
2016-04-24 12:25:37 +0200

    Load unique data, keep file on disk

--- node_helper.js
+++ node_helper.js
@@ -5,8 +5,14 @@ const ical = require("ical-generator");
 const Datastore = require("nedb");
 
 var instance = icloud();
-var phonenumberDB = new Datastore();
-var birthdayDB = new Datastore();
+var phonenumberDB = new Datastore({timestampData: true});
+phonenumberDB.ensureIndex({fieldName: "createdAt", expireAfterSeconds: 3600}, function(err) {});
+phonenumberDB.ensureIndex({fieldName: "unique", unique: true}, function(err) {});
+var birthdayDB = new Datastore({timestampData: true});
+birthdayDB.ensureIndex({fieldName: "createdAt", expireAfterSeconds: 3600}, function(err) {});
+birthdayDB.ensureIndex({fieldName: "unique", unique: true}, function(err) {console.log(err);});
+var contactDB = new Datastore({filename: "contacts.db", autoload: true, timestampData: true});
+contactDB.ensureIndex({fieldName: "createdAt", expireAfterSeconds: 3600}, function(err) {});
 
 function sanatizePhoneNumber(number, countrycode) {
 	return number.replace(/[^+0-9]+/gi,"").replace(/^00/,"+").replace(/^0/, "+" + countrycode);
@@ -18,16 +24,27 @@ module.exports = NodeHelper.create({
 		this.started = false;
 		console.log("Starting helper: " + this.name);
 		this.expressApp.get("/" + this.name + "/birthdays", this.serverBirthdays);
+		this.loadDB();
 	},
 
+	loadDB: function() {
+		console.log("loading Database");
+		var self = this;
+		contactDB.find({"birthday": {$exists: true}}, function(err, docs) {
+			docs.forEach(function(contact) {
+				self.extractBirthday(contact);
+			});
+		});
+	},
 	extractBirthday: function(contact) {
 		if (contact.birthday.length == 10) {
-			birthday = Date.parse(contact.birthday);
+			var today = new Date();
+			var birthday = Date.parse(contact.birthday);
 			if (birthday == NaN) {
 				console.log(contact.firstName + " " + contact.lastName + " has invalid birthday: " + contact.birthday);
 			} else {
 				birthday = new Date(contact.birthday);
-				nextbirthday = new Date(contact.birthday);
+				var nextbirthday = new Date(contact.birthday);
 
 				nextbirthday.setFullYear(today.getFullYear());
 				if (today > nextbirthday) {
@@ -41,12 +58,10 @@ module.exports = NodeHelper.create({
 					// Only Day is entered
 					birthday.setFullYear(today.getFullYear());
 				}
-				diff = nextbirthday.getTime() - today.getTime();
-				days = Math.floor(diff / (1000 * 60 * 60 * 24));
-				//console.log(nextbirthday + ' ' + nextbirthday.getFullYear());
-				//console.log(birthday + ' ' + birthday.getFullYear());
-				age = nextbirthday.getFullYear() - birthday.getFullYear();
-				var info = {type: "birthday", diffdays: days, age: age, nextbirthday: nextbirthday, firstName: contact.firstName, middleName: contact.middleName, lastName: contact.lastName};
+				var diff = nextbirthday.getTime() - today.getTime();
+				var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+				var age = nextbirthday.getFullYear() - birthday.getFullYear();
+				var info = {unique: "birthday-" + contact.contactId, diffdays: days, age: age, nextbirthday: nextbirthday, firstName: contact.firstName, middleName: contact.middleName, lastName: contact.lastName};
 				birthdayDB.insert(info);
 				//console.log("added bday for: " + contact.firstName + ' ' + contact.lastName + ' ' + contact.birthday);
 			}
@@ -55,11 +70,11 @@ module.exports = NodeHelper.create({
 		}
 	},
 
-	extractPhoneNumber: function(contact) {
+	extractPhoneNumber: function(contact, self) {
 		contact.phones.forEach(function(number) {
-			var sanenumber = sanatizePhoneNumber(number.field, this.config.countrycode);
-			console.log(sanenumber + " added for " + contact.firstName);
-			var info = {number: sanenumber, label: number.label, firstName: contact.firstName, middleName: contact.middleName, lastName: contact.lastName};
+			var sanenumber = sanatizePhoneNumber(number.field, self.config.countrycode);
+			//console.log(sanenumber + " added for " + contact.firstName);
+			var info = {unique: sanenumber, number: sanenumber, label: number.label, firstName: contact.firstName, middleName: contact.middleName, lastName: contact.lastName};
 			phonenumberDB.insert(info);
 		});
 	},
@@ -88,13 +103,13 @@ module.exports = NodeHelper.create({
 					start: contact.nextbirthday,
 					allDay: true,
 					floating: true,
-					description: name + birthday,
+					summary: name + birthday,
 				});
-				console.log(contact.firstName + " " + contact.lastName + " has birthday in " + contact.diffdays + " days and turns " + contact.age);
+				//console.log(contact.firstName + " " + contact.lastName + " has birthday in " + contact.diffdays + " days and turns " + contact.age);
 			});
+			console.log("Serving iCal");
+			cal.serve(res);
 		});
-		console.log("Serving iCal");
-		cal.serve(res);
 	},
 
 	socketNotificationReceived: function(notification, payload) {
@@ -105,21 +120,31 @@ module.exports = NodeHelper.create({
 				var self = this;
 				this.started = true;
 
-				/*instance.login(this.config.username, this.config.password, function(err) {
+				contactDB.find({"phones": {$exists: true}}, function(err, docs) {
+					docs.forEach(function(contact) {
+						self.extractPhoneNumber(contact);
+					});
+				});
+
+				instance.login(this.config.username, this.config.password, function(err) {
 					if (err) return console.log("login failed");
 					instance.contacts(function(err, results) {
 						if (err) return console.log("failed to fetch contacts");
 						results.contacts.forEach(function(contact) {
-							//contactDB.update({ "contact.contactID": contact.contactID}, { contact }, {upsert: true });
-							if (contact.birthday) {
+							contactDB.update({"contactId": contact.contactId}, contact, {upsert: true});
+							if (contact.birthday != undefined) {
 								self.extractBirthday(contact);
 							}
-							if (contact.phones) {
-								self.extractPhoneNumber(contact);
+							if (contact.phones != undefined) {
+								self.extractPhoneNumber(contact, self);
 							}
 						});
+						contactDB.persistence.compactDatafile();
+						contactDB.count({}),function(err, count) {
+							console.log("Loaded " + count + " Contacts");
+						};
 					});
-				});*/
+				});
 			};
 		};
 	}
