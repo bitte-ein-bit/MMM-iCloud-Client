db6ad8a51e0dc985da6f70f16d86d19ad0b89e0e
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-12-11 22:55:39 +0100

    Format

--- MMM-iCloud-Client.js
+++ MMM-iCloud-Client.js
@@ -1,14 +1,13 @@
 /* global Module */
 
 /* MagicMirror²
-* Module: MMM-iCloud-Client
-*
-* By Jonathan Vogt https://github.com/bitte-ein-bit
-* MIT Licensed.
-*/
+ * Module: MMM-iCloud-Client
+ *
+ * By Jonathan Vogt https://github.com/bitte-ein-bit
+ * MIT Licensed.
+ */
 
-Module.register("MMM-iCloud-Client",{
-
+Module.register("MMM-iCloud-Client", {
 	// Default module config.
 	defaults: {
 		username: "",
@@ -16,28 +15,35 @@ Module.register("MMM-iCloud-Client",{
 		countrycode: "49",
 	},
 
-	start: function() {
+	start: function () {
 		//Open Socket connection
 		this.sendSocketNotification("CONFIG", this.config);
-		Log.info("Starting module: " + this.name);
+		Log.info(`Starting module: ${this.name}`);
 	},
 
-	notificationReceived: function(notification, payload, sender) {
+	notificationReceived: function (notification, payload, sender) {
 		if (sender) {
-			Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
+			Log.log(
+				`${this.name} received a module notification: ${notification} from sender: ${sender.name}`,
+			);
 		} else {
-			Log.log(this.name + " received a system notification: " + notification);
+			Log.log(`${this.name} received a system notification: ${notification}`);
 		}
 		if (notification === "PHONE_LOOKUP") {
-			this.sendSocketNotification("PHONE_LOOKUP", {number: payload.number, reason: payload.reason, sender: sender.name});
+			this.sendSocketNotification("PHONE_LOOKUP", {
+				number: payload.number,
+				reason: payload.reason,
+				sender: sender.name,
+			});
 		}
 	},
 
-	socketNotificationReceived: function(notification, payload) {
-		Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+	socketNotificationReceived: function (notification, payload) {
+		Log.log(
+			`${this.name} received a socket notification: ${notification} - Payload: ${payload}`,
+		);
 		if (notification === "PHONE_LOOKUP_RESULT") {
 			this.sendNotification("PHONE_LOOKUP_RESULT", payload);
 		}
 	},
-
 });
